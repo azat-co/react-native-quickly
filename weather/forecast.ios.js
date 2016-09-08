0bb880564809352c6350143a53f417680d3d3d0f
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-09-09 02:03:01 +0400

    cleanups

--- weather/forecast.ios.js
+++ weather/forecast.ios.js
@@ -9,30 +9,31 @@ var {
   ListView,
   ScrollView
 } = React
-
-const cToF = (c) => {
+var cToF = (c) => {
   return Math.round((c*1.8 + 32)*100)/100
 }
 
-const ForecastRow = (forecast)=> {
+var ForecastRow = (forecast)=> {
   return (
     <View style={styles.row}>
       <View style={styles.rightContainer}>
-        <Text style={styles.subtitle}></Text>
+        <Text style={styles.subtitle} />
         <Text style={styles.subtitle}>
           {forecast.dt_txt}: {forecast.weather[0].description}, {forecast.main.temp}C/{cToF(forecast.main.temp)}F
         </Text>
-       </View>
+      </View>
     </View>
   )
 }
 
 module.exports = React.createClass({
-  render: function() {
+  render() {
     return (
       <ScrollView style={styles.scroll}>
         <Text style={styles.text}>{this.props.forecastRaw.city.name}</Text>
-        <ListView dataSource={this.props.forecastData} renderRow={ForecastRow} style={styles.listView}/>
+        <ListView
+          dataSource={this.props.forecastData}
+          renderRow={ForecastRow} style={styles.listView} />
       </ScrollView>
     )
   }
