57e50cf11bb10979cc1efbe3de1e990be4ef8b7f
Robin Kuehn <kuehn.robin@gmail.com>
2017-02-22 21:58:50 -0800

    add android configurrations for react-native-audioplayer

--- timer/index.android.js
+++ timer/index.android.js
@@ -2,51 +2,124 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
+'use strict'
 
-var React = require('react-native');
+var React = require('react-native'),
+  AudioPlayer = require('react-native-audioplayer')
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-} = React;
+  ScrollView,
+  TouchableOpacity,
+  Switch
+} = React
 
-var timer = React.createClass({
-  render: function() {
+const timerOptions = [5, 7, 10, 12, 15, 21]
+
+var TimerWrapper = React.createClass({
+  getInitialState () {
+    return {time: null, int: null, isMinutes: false}
+  },
+  toggleTime(){
+    let time = this.state.time
+    if (time == 0 ) time = null
+    this.setState({isMinutes: !this.state.isMinutes, time: time})
+  },
+  startTimer(time) {
+    clearInterval(this.state.int)
+    var _this= this
+    var int = setInterval(function() {
+      console.log('2: Inside of setInterval')
+      var tl = _this.state.time - 1
+      if (tl == 0) clearInterval(int)
+      _this.setState({time: tl})
+    }, 1000)
+    console.log('1: After setInterval')
+    return this.setState({time: time, int: int})
+  },
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.heading}>Timer</Text>
+          <Text style={styles.instructions}>Press a button</Text>
+          <View style={styles.buttons}>
+            {timerOptions.map((item, index, list)=>{
+              return <Button key={index} time={item} startTimer={this.startTimer} isMinutes={this.state.isMinutes}/>
+            })}
+          </View>
+          <Text>Minutes</Text>
+          <Switch onValueChange={this.toggleTime} value={this.state.isMinutes}></Switch>
+          <Timer time={this.state.time}/>
+        </View>
+      </ScrollView>
+    )
   }
-});
+})
+
+var Button = React.createClass({
+  startTimer(event) {
+    let time = (this.props.isMinutes) ? this.props.time*60 : this.props.time
+    return this.props.startTimer(time)
+  },
+  render() {
+    return (
+      <TouchableOpacity onPress={this.startTimer}>
+        <Text style={styles.button}>{this.props.time} {(this.props.isMinutes) ? 'minutes' : 'seconds'}</Text>
+      </TouchableOpacity>
+    )
+  }
+})
+
+var Timer = React.createClass({
+   render() {
+     if (this.props.time == 0) {
+      AudioPlayer.play('flute_c_long_01.wav')
+     }
+     if (this.props.time == null || this.props.time == 0) return <View><Text  style={styles.heading}> </Text></View>
+     return (
+       <View>
+         <Text style={styles.heading}>{this.props.time}</Text>
+         <Text>Seconds left</Text>
+       </View>
+     )
+    }
+})
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  heading: {
+    flex: 1,
+    fontSize: 36,
+    paddingTop: 40,
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 15,
   },
-});
+  button: {
+    color: '#111',
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: 'blue',
+    padding: 10,
+    borderRadius: 20,
+    fontWeight: '600'
+  },
+  buttons: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  }
+})
 
-AppRegistry.registerComponent('timer', () => timer);
+
+AppRegistry.registerComponent('timer', () => TimerWrapper)
